966ded56c9774360e7fbf603f7fe0350f2854f56
RemaLuhaim <Rema.luhaim@gmail.com>
2026-05-17 21:52:34 +0300

    First commit of all Clinic System

---
+++ Clinic_APIs/Models/AppointmentsStatus.cs
@@ -0,0 +1,15 @@
+using system;
+using system.Collections.Generic;
+
+
+namespace Clinic_APIs.Controllers{
+public enum AppointmentStatus{
+
+    Scheduled, 
+    Completed,
+    Canceled, 
+   
+}
+
+}
+ 
